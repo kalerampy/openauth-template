4a788f78bd31bed4927203c5a823433b901b64a8
Kale Rampenthal <kalerampenthal@gmail.com>
2025-11-08 21:25:44 -0500

    updated theme

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { PasswordProvider } from "@openauthjs/openauth/provider/password";
 import { PasswordUI } from "@openauthjs/openauth/ui/password";
 import { createSubjects } from "@openauthjs/openauth/subject";
 import { object, string } from "valibot";
+import { THEME_VERCEL } from "@openauthjs/openauth/ui/theme";
 
 // This value should be shared between the OpenAuth server Worker and other
 // client Workers that you connect to it, so the types and schema validation are
@@ -58,16 +59,7 @@ export default {
           }),
         ),
       },
-      theme: {
-        title: "myAuth",
-        primary: "#0051c3",
-        favicon: "https://workers.cloudflare.com//favicon.ico",
-        logo: {
-          dark: "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/db1e5c92-d3a6-4ea9-3e72-155844211f00/public",
-          light:
-            "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/fa5a3023-7da9-466b-98a7-4ce01ee6c700/public",
-        },
-      },
+      theme: THEME_VERCEL,
       success: async (ctx, value) => {
         return ctx.subject("user", {
           id: await getOrCreateUser(env, value.email),
